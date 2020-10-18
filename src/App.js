4d10190ec601f5f0ee8003d2101eca206a42ff1a
Tykhonchuk <tykhonchukivan@gmail.com>
2020-10-18 20:48:47 +0300

    pref: practice useMemo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import UseStateComponent from "./UseStateComponent/UseStateComponent";
-import UseEffectComponent from "./UseEffectComponent/UseEffectComponent";
-import UseRefComponent from "./UseRefComponent/UseRefComponent";
+import UseStateComponent from './UseStateComponent/UseStateComponent';
+import UseEffectComponent from './UseEffectComponent/UseEffectComponent';
+import UseRefComponent from './UseRefComponent/UseRefComponent';
+import UseMemoComponent from './UseMemoComponent/UseMemoComponent';
 
 const App = props => {
   return (
     <>
       <UseStateComponent/>
       <UseEffectComponent/>
-      <UseRefComponent />
+      <UseRefComponent/>
+      <UseMemoComponent/>
     </>
   )
 }
